4192b921fecb5ea26ac8c5a636436c78241f3a50
Fernando <fptcorrea@gmail.com>
2023-06-20 12:27:52 -0300

    aula xgboost e random forest

---
+++ exemplos_de_aula/rf_e_xbg_na_mao.R
@@ -0,0 +1,93 @@
+library(tree)
+library(magrittr)
+library(ggplot2)
+library(tidyverse)
+x <- runif(500, -1, 1)
+y <- sin(x*3) + rnorm(100, sd = 0.1)
+
+dados <- tibble(x, y)
+
+dados %>%
+  ggplot(aes(x = x, y = y)) +
+  geom_point()
+
+
+# random forest -----------------------------------------------------------
+
+arvores <- list()
+trees <- 500
+tree_depth <- 30
+mtry <- 1
+
+n <- nrow(dados)
+features <- setdiff(names(dados), "y")
+for(i in 1:trees) {
+  print(i)
+  amostra_bootstrap <- dados[sample.int(n, n, replace = TRUE), c("y", sample(features, mtry))]
+  arvores[[i]] <- rpart::rpart(y ~ .,
+                               data = amostra_bootstrap,
+                               control = rpart::rpart.control(maxdepth = tree_depth))
+}
+
+f <- function(x, arvores) {
+  trees <- length(arvores)
+  pred <- rep(0, length = length(x))
+  for(i in 1:trees){
+    pred <- pred + predict(arvores[[i]], tibble(x = x))
+  }
+  return(pred/trees)
+}
+
+dados %>%
+  mutate(pred_rf = f(x, arvores)) %>%
+  ggplot() +
+  geom_point(aes(x = x, y = y)) +
+  geom_step(aes(x = x, y = pred_rf, colour = "RF"), size = 1)
+
+# boosting ----------------------------------------------------------------
+
+loss <- function(y, y_hat) (y - y_hat)^2
+
+# gradiente (G)
+G <- function(y, y_hat) - 2 * (y - y_hat)
+
+# hessiana (H)
+H <- function(y, y_hat) 2
+
+
+# f(x) = a + b*x
+# f(x, arvores) = 0.0 + lr * arvore1 + lr * arvore2 + ... + lr * arvoreN
+f <- function(x, arvores) {
+  r <- rep(0, length(x))
+
+  # soma as árvores (os case_whens)
+  for (arvore in arvores) {
+    r <- r + lr * predict(arvore, tibble(x = x))
+  }
+  r
+}
+
+arvores <- list()
+y_hat <- 0.5
+lr <- 0.5
+trees <- 50
+lambda <- 10
+gamma <- 40
+tree_depth <- 3
+
+for (i in 1:trees) {
+  print(i)
+  r <- -G(y, y_hat)/(H(y, y_hat) + lambda) # output = - G/H
+  # no caso de loss = (y-y_hat)^2 - r é (y-y_hat)/(2+lambda)
+  arvores[[i]] <-  rpart::rpart(r ~ x,
+                                control = rpart::rpart.control(maxdepth = tree_depth))
+  y_hat <- f(x, arvores)
+}
+
+plot(x, y)
+points(x, y_hat, col = 'red')
+
+tibble(x = x, y = y, y_hat = y_hat) %>%
+  ggplot() +
+  geom_point(aes(x = x, y = y)) +
+  geom_step(aes(x = x, y = y_hat), colour = "red", size = 1)
